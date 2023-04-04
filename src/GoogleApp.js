5326a5e96be57944f8d624e3fb83c3f06d845b1b
julietoge <julietogechi27@gmail.com>
2023-04-04 20:39:30 +0100

    updated header & added break down analysis

---
+++ src/GoogleApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeaderComponent from './mainComponents/HeaderComponents/HeaderComponent'
+
+function GoogleApp() {
+  return (
+    <>
+    <HeaderComponent />
+    </>
+  )
+}
+
+export default GoogleApp;
---
+++ src/mainComponents/Main-Section/MainSectionComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MainSectionComponent() {
+  return (
+    <div>MainSectionComponent</div>
+  )
+}
